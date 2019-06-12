94d05bfac17fb2520a91f209fdd3571e90ee3e2d
MVasanthiM <51661449+MVasanthiM@users.noreply.github.com>
2019-06-12 10:34:02 +0530

    Create vowels(c)(inva).py

---
+++ programs/vowels(c)(inva).py
@@ -0,0 +1,8 @@
+v=input()
+if(v=='a' or v=='A' or v=='e' or v=='E' or v=='i' or v=='I' or v=='o' or v=='O' or v=='u' or v=='U'):
+  print("vowel")
+elif v.isalpha():
+  print("consonant")
+else:
+    print("invalid")
+
